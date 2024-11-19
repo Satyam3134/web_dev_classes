06ddd8ba39d9c0d83afdd7d593755d8e7c37ea93
Satyam34 <satyam3100@gmail.com>
2024-11-19 22:20:51 +0530

    reactjs setup ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Footer, Header, NavBar } from "./components";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,3 @@
+export {default as NavBar} from "./navbar/navBar.jsx";
+export {default as Header} from "./header/header.jsx";
+export {default as Footer} from "./footer/footer.jsx";
